ded118c8be5ebf252ed348cc14a71456ab681033
Andreas Lindhé <andreas@lindhe.io>
2020-01-19 21:11:57 +0100

    Short-click lsft for '

--- layouts/community/ergodox/swedish-lindhe/keymap.c
+++ layouts/community/ergodox/swedish-lindhe/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------+------+------+------+------+------|      |       |      |------+------+------+------+------+-----------|
  * | CapsLk    | A    | S    | D    | F    | G    |------|       |------| H    | J    | K    | L    | Ö    | Ä         |
  * |-----------+------+------+------+------+------| `    |       | Del  |------+------+------+------+------+-----------|
- * | LShft     | Z    | X    | C    | V    | B    |  '   |       |      | N    | M    | ,    | .    | -    | RShift (')|
+ * | LShft (') | Z    | X    | C    | V    | B    |  '   |       |      | N    | M    | ,    | .    | -    | RShift (')|
  * `-----------+------+------+------+------+-------------'       `-------------+------+------+------+------+-----------'
  *   | LCtl    |  ^   | *    | LAlt | LGui |                                   | AltGr| Down |  Up  | Left | Right|
  *   |         |  " ~ | '  ´ |      |      |                                   |      |      |      |      |      |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PSCR,         KC_1,     KC_2,     KC_3,    KC_4,    KC_5,   KC_F11,
     KC_TAB,          KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,   MO(SYMB),
     KC_CAPS,         KC_A,     KC_S,     KC_D,    KC_F,    KC_G,
-    KC_LSFT,         KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,   NO_ACUT,
+    LSFT_T(NO_APOS), KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,   NO_ACUT,
     KC_LCTRL,        NO_CIRC,  NO_ASTR,  KC_LALT, KC_LGUI,
                                                KC_LCTRL,  KC_LALT,
                                                           NO_TILD,
