77e4c39f83e95dc45567cac32e7ff97bfd0b93fd
Andreas Lindhé <andreas@lindhe.io>
2020-08-26 20:44:22 +0200

    Set TAPPING_TOGGLE to 2

--- layouts/community/ergodox/swedish-lindhe/config.h
+++ layouts/community/ergodox/swedish-lindhe/config.h
@@ -4,3 +4,8 @@
 #undef TAPPING_TERM
 #endif // TAPPING_TERM
 #define TAPPING_TERM 150 // default: 200
+
+#ifdef TAPPING_TOGGLE
+#undef TAPPING_TOGGLE
+#endif // TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
